200b8e21533e96daa4713413ac3f0e40cdbec1db
khaihoang <khaihoang119nkh@gmail.com>
2023-11-17 00:21:38 +0700

    NO-01 chỉnh sửa giao diện

---
+++ Arsha/index.php
@@ -0,0 +1,11 @@
+<?php
+    include './view/header.php';
+
+    if((!isset($_GET['act'])) && $_GET['act'] !=""){
+        $act =$_GET['act'];
+        switch ($act){
+            case'':
+                break;
+        }
+    }
+?>
