eab033872a7adaa836977fdd3a83599fe23d9e2f
khaihoang <khaihoang119nkh@gmail.com>
2023-11-21 01:54:05 +0700

    NO-01 chỉnh sửa giao diện product

--- Arsha/view/product.php
+++ Arsha/view/product.php
@@ -9,25 +9,6 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <link href="../assets/img/favicon.png" rel="icon">
-  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="../assets/css/style.css" rel="stylesheet">
-
   <!-- =======================================================
   * Template Name: Arsha
   * Updated: Sep 18 2023 with Bootstrap v5.3.2
@@ -60,9 +41,32 @@
 
     <section class="inner-page">
       <div class="container">
-        <p>
-          Example inner page template
-        </p>
+        <div class="row justify-content-center text-center">
+            <div class="row">
+                <div class="col-md-6 col-lg-4 col-xl-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55 76KG</p>
+                            <div class="card " style="width: 10rem;">
+
+                                <div class="card-body">
+                                    <p class="card-text">I9-13980HX</p>
+                                    <p class="card-text">RTX 4090</p>
+                                    <p class="card-text">32GB</p>
+                                    <p class="card-text">1 TB</p>
+                                    <p class="card-text">15.6 inch</p>
+                                    <p class="card-text">165Hz</p>
+                                </div>
+
+                            </div>
+                            <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                            <p class="card-text">98.000.000 đ</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     </section>
 
@@ -155,17 +159,7 @@
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
-  <script src="../assets/vendor/aos/aos.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
-  <script src="../assets/js/main.js"></script>
 
 </body>
 
