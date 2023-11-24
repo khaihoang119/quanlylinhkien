78b59ac4e659423c7197cfaf9677b9647c9df65a
khang <giakhangmn0@gmail.com>
2023-11-25 01:17:23 +0700

    No2 - admin update

--- ql_shoplinhkien.sql
+++ ql_shoplinhkien.sql
@@ -3,13 +3,15 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 27, 2023 at 04:35 AM
+-- Generation Time: Nov 24, 2023 at 04:59 PM
 -- Server version: 8.0.31
 -- PHP Version: 7.4.33
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET
+time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,15 +29,17 @@ SET time_zone = "+00:00";
 -- Table structure for table `account`
 --
 
-CREATE TABLE `account` (
-  `userID` int NOT NULL,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `fullName` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `phoneNumber` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `role` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `status` int NOT NULL
+CREATE TABLE `account`
+(
+    `userID`      int                                                           NOT NULL,
+    `username`    varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `email`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `password`    varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `lastName`    varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `firstName`   varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `phoneNumber` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL,
+    `address`     varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `role`        varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -44,12 +48,13 @@ CREATE TABLE `account` (
 -- Table structure for table `bills`
 --
 
-CREATE TABLE `bills` (
-  `billID` int NOT NULL,
-  `userID` int DEFAULT NULL,
-  `orderDate` datetime DEFAULT NULL,
-  `pricetoPay` double NOT NULL,
-  `status` int NOT NULL
+CREATE TABLE `bills`
+(
+    `billID`    int NOT NULL,
+    `userID`    int      DEFAULT NULL,
+    `orderDate` datetime DEFAULT NULL,
+    `pricetoPay` double NOT NULL,
+    `status`    int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -58,10 +63,11 @@ CREATE TABLE `bills` (
 -- Table structure for table `bills_product`
 --
 
-CREATE TABLE `bills_product` (
-  `billID` int NOT NULL,
-  `productID` int NOT NULL,
-  `quantity` int NOT NULL
+CREATE TABLE `bills_product`
+(
+    `billID`    int NOT NULL,
+    `productID` int NOT NULL,
+    `quantity`  int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -70,19 +76,18 @@ CREATE TABLE `bills_product` (
 -- Table structure for table `categories`
 --
 
-CREATE TABLE `categories` (
-  `categoryID` int NOT NULL,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `status` int NOT NULL
+CREATE TABLE `categories`
+(
+    `categoryID` int                                                          NOT NULL,
+    `name`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`categoryID`, `name`, `status`) VALUES
-(1, 'Bàn phím', 1),
-(2, 'Màn hình', 1);
+INSERT INTO `categories` (`categoryID`, `name`)
+VALUES (15, 'Bàn Phím');
 
 -- --------------------------------------------------------
 
@@ -90,12 +95,13 @@ INSERT INTO `categories` (`categoryID`, `name`, `status`) VALUES
 -- Table structure for table `comments`
 --
 
-CREATE TABLE `comments` (
-  `commentID` int NOT NULL,
-  `userID` int NOT NULL,
-  `productID` int NOT NULL,
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `commentDate` datetime DEFAULT NULL
+CREATE TABLE `comments`
+(
+    `commentID`   int NOT NULL,
+    `userID`      int NOT NULL,
+    `productID`   int NOT NULL,
+    `content`     text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+    `commentDate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -104,24 +110,25 @@ CREATE TABLE `comments` (
 -- Table structure for table `products`
 --
 
-CREATE TABLE `products` (
-  `productID` int NOT NULL,
-  `productName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `image` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `productPrice` double NOT NULL,
-  `shortDes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `longDes` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
-  `categoryID` int NOT NULL,
-  `status` int NOT NULL
+CREATE TABLE `products`
+(
+    `productID`   int                                                          NOT NULL,
+    `productName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `image`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+    `productPrice` double NOT NULL,
+    `shortDes`    text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+    `longDes`     text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+    `categoryID`  int                                                          NOT NULL,
+    `status`      int                                                          NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`productID`, `productName`, `image`, `productPrice`, `shortDes`, `longDes`, `categoryID`, `status`) VALUES
-(3, 'Bàn Phím CMK75', 'banphim01', 2000000, 'Bàn Phím CMK', 'Bàn Phím CMK 75 của hãng FLC', 1, 1),
-(4, 'Màn Hình Cong MSI Optix MAG321CQR 144Hz', 'manhinh01', 10000000, 'Màn hình cong', 'Màn hình cong của hãng msi', 2, 1);
+INSERT INTO `products` (`productID`, `productName`, `image`, `productPrice`, `shortDes`, `longDes`, `categoryID`,
+                        `status`)
+VALUES (6, 'Bàn Phím Cơ ', 'OIP.jpg', 25000000, 'ngon', 'rất ngon', 15, 1);
 
 --
 -- Indexes for dumped tables
@@ -131,33 +138,33 @@ INSERT INTO `products` (`productID`, `productName`, `image`, `productPrice`, `sh
 -- Indexes for table `account`
 --
 ALTER TABLE `account`
-  ADD PRIMARY KEY (`userID`);
+    ADD PRIMARY KEY (`userID`);
 
 --
 -- Indexes for table `bills`
 --
 ALTER TABLE `bills`
-  ADD PRIMARY KEY (`billID`),
+    ADD PRIMARY KEY (`billID`),
   ADD KEY `userID` (`userID`);
 
 --
 -- Indexes for table `bills_product`
 --
 ALTER TABLE `bills_product`
-  ADD KEY `billID` (`billID`),
+    ADD KEY `billID` (`billID`),
   ADD KEY `productID` (`productID`);
 
 --
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`categoryID`);
+    ADD PRIMARY KEY (`categoryID`);
 
 --
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`commentID`),
+    ADD PRIMARY KEY (`commentID`),
   ADD KEY `userID` (`userID`),
   ADD KEY `productID` (`productID`);
 
@@ -165,7 +172,7 @@ ALTER TABLE `comments`
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`productID`),
+    ADD PRIMARY KEY (`productID`),
   ADD KEY `categoryID` (`categoryID`);
 
 --
@@ -176,31 +183,31 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `userID` int NOT NULL AUTO_INCREMENT;
+    MODIFY `userID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `bills`
 --
 ALTER TABLE `bills`
-  MODIFY `billID` int NOT NULL AUTO_INCREMENT;
+    MODIFY `billID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `categoryID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+    MODIFY `categoryID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `commentID` int NOT NULL AUTO_INCREMENT;
+    MODIFY `commentID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `productID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+    MODIFY `productID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -210,27 +217,27 @@ ALTER TABLE `products`
 -- Constraints for table `bills`
 --
 ALTER TABLE `bills`
-  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `account` (`userID`);
+    ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `account` (`userID`);
 
 --
 -- Constraints for table `bills_product`
 --
 ALTER TABLE `bills_product`
-  ADD CONSTRAINT `bills_product_ibfk_1` FOREIGN KEY (`billID`) REFERENCES `bills` (`billID`),
+    ADD CONSTRAINT `bills_product_ibfk_1` FOREIGN KEY (`billID`) REFERENCES `bills` (`billID`),
   ADD CONSTRAINT `bills_product_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
 
 --
 -- Constraints for table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `account` (`userID`),
+    ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `account` (`userID`),
   ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
 
 --
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`categoryID`);
+    ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`categoryID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
