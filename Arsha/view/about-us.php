d1395c76afd5373fb660e07196cd49e525756cfe
khaihoang <khaihoang119nkh@gmail.com>
2023-11-28 06:29:06 +0700

    làm lại từ đầu

---
+++ Arsha/view/about-us.php
@@ -0,0 +1,311 @@
+
+<section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+        <div class="section-title mt-4">
+            <h2>Về Chúng Tôi </h2>
+        </div>
+
+        <div class="row content">
+            <div class="col-lg-6">
+                <p>
+                    GearSetupvn nhà cung cấp, bán lẻ sản phẩm chính hãng chúng có gì
+                </p>
+                <ul>
+                    <li><i class="ri-check-double-line"></i>Cam kết đảm bảo những sản phẩm đến tay khách hàng đều là hàng chính hãng</li>
+                    <li><i class="ri-check-double-line"></i>Chính sách bảo hành sản phẩm tận tâm</li>
+                    <li><i class="ri-check-double-line"></i>Mức giá ưu đãi cạnh tranh với thị trường</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- ======= Services Section ======= -->
+<section id="services" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Dịch vụ của chúng tôi</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon-box">
+                    <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                    <h4><a href="">Tư vấn cấu hình, linh kiện</a></h4>
+                    <p>Hỗ trợ tư vấn khách hàng để phù hợp với nhu cầu sử dụng của khách</p>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+                <div class="icon-box">
+                    <div class="icon"><i class="bx bx-file"></i></div>
+                    <h4><a href="">Cung cấp các sản phẩm, linh kiện</a></h4>
+                    <p>Kinh doanh các mặt hàng sản phẩm thiết bị điện tử mới còn bảo hành cho khách hàng</p>
+
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
+                <div class="icon-box">
+                    <div class="icon"><i class="bx bx-tachometer"></i></div>
+                    <h4><a href="">Sửa chữa linh kiện, sản phẩm</a></h4>
+                    <p>Nhận sửa chữa các thiết bị linh kiện điện tử bị hư hỏng</p>
+                </div>
+            </div>
+
+            <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
+                <div class="icon-box">
+                    <div class="icon"><i class="bx bx-layer"></i></div>
+                    <h4><a href="">Hỗ trợ lắp ráp linh kiện cho khách hàng</a></h4>
+                    <p>Lắp ráp, vận chuyển tận nơi cho khách hàng</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+<!-- End Services Section -->
+<!-- ======= Why Us Section ======= -->
+<section id="why-us" class="why-us section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="row">
+
+            <div class="col-lg-12 d-flex flex-column align-items-stretch order-2 order-lg-1">
+
+                <div class="content">
+                    <h3>Chính Sách Bảo Hành</h3>
+
+                </div>
+
+                <div class="accordion-list">
+                    <ul>
+                        <li>
+                            <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-1"><span>01</span> <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                            <div id="accordion-list-1" class="collapse show" data-bs-parent=".accordion-list">
+                                <p>
+                                   Khi có nhu cầu bảo hành sản phẩm, khách hàng vui lòng liên hệ với GearSetup như sau
+                                </p>
+                                <p>
+                                    Tổng đài bảo hành: 0123456789
+                                </p>
+                                <p>
+                                    Trò chuyện với GearSetup trực tiếp tại cửa hàng hoặc qua Fanpage
+                                </p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-2" class="collapsed"><span>02</span> <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                            <div id="accordion-list-2" class="collapse" data-bs-parent=".accordion-list">
+                                <p>
+                                  Quý khách mang sản phẩm cần được bảo hành tới trực tiếp tại các showroom của GearSetup hoặc gửi hàng qua bưu điện
+                                </p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-3" class="collapsed"><span>03</span> <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                            <div id="accordion-list-3" class="collapse" data-bs-parent=".accordion-list">
+                                <p>
+                                    Hoàn tất xử lí bảo hành và gửi trả sản phẩm về tay khách hàng
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+
+            <div class="col-lg-12 d-flex flex-column align-items-stretch order-2 order-lg-1">
+
+                <div class="content">
+                    <h3>Điều kiện bảo hành</h3>
+
+                </div>
+
+                <div class="accordion-list">
+                    <ul>
+                        <li>
+                            <a data-bs-toggle="collapse" class="collapse" data-bs-target="#accordion-list-4"><span>01</span>Những sản phẩm đủ điều kiện bảo hành <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                            <div id="accordion-list-4" class="collapse show" data-bs-parent=".accordion-list">
+                                <p>
+                                    • Sản phẩm nếu có tem niêm phong (seal) trên sản phẩm thì tem niêm phong phải còn nguyên vẹn.
+                                </p>
+                                <p>
+                                    • Dữ liệu (lưu trữ trong sản phẩm: Laptop/ Máy tính để bàn/ Thẻ nhớ/ Ổ cứng…) không thuộc phạm vi bảo hành. Chúng tôi hoàn toàn ý thức được tầm quan trọng của dữ liệu của quý khách hàng và luôn cố gắng hết sức để hỗ trợ - hướng dẫn quý khách hàng trong việc sao lưu dữ liệu. Gearvn không chịu trách nhiệm về bất cứ thiệt hại trực tiếp hoặc gián tiếp nào gây ra cho quý khách hàng nếu dữ liệu lưu trong sản phẩm bị tiết lộ, bị mất, bị hư hỏng trong quá trình kiểm tra, xử lý bảo hành.
+                                </p>
+                                <p>
+                                    • Đối với màn hình Laptop và màn hình rời, lỗi điểm chết màn hình là khi màn hình có từ 5 điểm chết trở lên (riêng với Laptop Ideapad là trên 6 điểm)
+                                </p>
+                                <p>
+                                    • Đối với sản phẩm bảo hành trên hộp: sản phẩm còn đầy đủ hộp.
+                                </p>
+                                <p>
+                                    • Sản phẩm không trầy xước, cấn móp, bể, vỡ, biến dạng so với ban đầu.
+                                </p>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a data-bs-toggle="collapse" data-bs-target="#accordion-list-5" class="collapsed"><span>02</span>Những sản phẩm không đủ điều kiện bảo hành <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+                            <div id="accordion-list-5" class="collapse" data-bs-parent=".accordion-list">
+                                <p>
+                                    • Hết thời hạn bảo hành.
+                                </p>
+                                <p>
+                                    • Không có tem niêm phong, hoặc bị tẩy xóa, không còn nguyên dạng ban đầu.
+                                </p>
+                                <p>
+                                    • Bị tác động vật lý làm trầy xước, cong vênh, rạn nứt, bể vỡ trong quá trình quá trình sử dụng.
+                                </p>
+                                <p>
+                                    • Bị hư hỏng do tự ý thảo mở, sửa chữa, thay đổi cấu trúc sản phẩm bên trong mà chưa có sự xác nhận đồng ý hoặc giám sát bởi kỹ thuật viên GearVN.
+                                </p>
+                                <p>
+                                    • Bị hư hỏng, chập, cháy do sử dụng sai mục đích, tự ý tháo, lắp đặt không tuân theo các hướng dẫn sử dụng đính kèm theo sản phẩm.
+                                </p>
+                                <p>
+                                    • Bị hư hỏng do côn trùng xâm nhập (chuột, gián, kiến, mối…)
+                                </p>
+                                <p>
+                                    • Bị hư hỏng do thiên tai, hỏa hoạn, lụt lội, sét đánh, rỉ sét, hao mòn do môi trường gây ra.
+                                </p>
+                                <p>
+                                    GearSetup hỗ trợ tiếp nhận & sửa chữa sản phẩm (Có thu phí đối với khách hàng) với những sản phẩm không đủ điều kiện bảo hành.
+                                </p>
+                            </div>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+            <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("../assets/img/why-us.png");' data-aos="zoom-in" data-aos-delay="150">&nbsp;</div>
+        </div>
+
+    </div>
+</section>
+<!-- ======= Team Section ======= -->
+<section id="team" class="team section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Team</h2>
+              </div>
+
+        <div class="row">
+
+            <div class="col-lg-6" data-aos="zoom-in" data-aos-delay="100">
+                <div class="member d-flex align-items-start">
+                    <div class="pic"><img src="./assets/img/team/khaihoang.jpg" class="img-fluid" alt=""></div>
+                    <div class="member-info">
+                        <h4>Nguyễn Khải Hoàng</h4>
+                        <span>Trưởng Nhóm</span>
+                        <div class="social">
+                            <a href=""><i class="ri-twitter-fill"></i></a>
+                            <a href="https://www.facebook.com/KhaiHoangJohnny/"><i class="ri-facebook-fill"></i></a>
+                            <a href=""><i class="ri-instagram-fill"></i></a>
+                            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="200">
+                <div class="member d-flex align-items-start">
+                    <div class="pic"><img src="./assets/img/team/giakhang.jpg" class="img-fluid" alt=""></div>
+                    <div class="member-info">
+                        <h4>Hồng Võ Gia Khang</h4>
+                        <span>Thành Viên</span>
+                        <div class="social">
+                            <a href=""><i class="ri-twitter-fill"></i></a>
+                            <a href="https://www.facebook.com/Gkhaq"><i class="ri-facebook-fill"></i></a>
+                            <a href=""><i class="ri-instagram-fill"></i></a>
+                            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+</section><!-- End Team Section -->
+<!-- ======= Contact Section ======= -->
+<section id="contact" class="contact">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>Liên Hệ</h2>
+            <p></p>
+        </div>
+
+        <div class="row">
+
+            <div class="col-lg-5 d-flex align-items-stretch">
+                <div class="info">
+                    <div class="address">
+                        <i class="bi bi-geo-alt"></i>
+                        <h4>Địa Chỉ</h4>
+                        <p>Đ.Số 22, Thường Thạnh, Cái Răng, Cần Thơ
+                            Việt Nam</p>
+                    </div>
+
+                    <div class="email">
+                        <i class="bi bi-envelope"></i>
+                        <h4>Email:</h4>
+                        <p>gearsetupvn@example.com</p>
+                    </div>
+
+                    <div class="phone">
+                        <i class="bi bi-phone"></i>
+                        <h4>Phone:</h4>
+                        <p>+89 0123456789</p>
+                    </div>
+
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+            <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="name">Tên Bạn</label>
+                            <input type="text" name="name" class="form-control" id="name" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="name">Email</label>
+                            <input type="email" class="form-control" name="email" id="email" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Chủ đề</label>
+                        <input type="text" class="form-control" name="subject" id="subject" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Lời nhắn</label>
+                        <textarea class="form-control" name="message" rows="10" required></textarea>
+                    </div>
+                    <div class="my-3">
+                        <div class="loading">Loading</div>
+                        <div class="error-message"></div>
+                        <div class="sent-message">Đơn hỗ trợ của bạn đã được gửi. Cảm ơn bạn!</div>
+                    </div>
+                    <div class="text-center"><button type="submit">Gửi</button></div>
+                </form>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+<!-- End Contact Section -->
+
---
+++ Arsha/view/product.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Trang sản phẩm</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- =======================================================
+    * Template Name: Arsha
+    * Updated: Sep 18 2023 with Bootstrap v5.3.2
+    * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+
+<!-- End Header -->
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container mt-2">
+            <ol>
+                <li><a href="home.php">Trang Chủ</a></li>
+                <li>Sản Phẩm</li>
+            </ol>
+            <h2>Sản Phẩm</h2>
+
+        </div>
+    </section><!-- End Breadcrumbs -->
+    <section class="inner-page">
+        <div class="container" data-aos="fade-up" data-aos-delay="200">
+            <div class="row justify-content-center text-start">
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card" style="width: 18rem;">
+                          <a href="index.php?act=product-detail"><img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="..."></a>
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold "> <a href="index.php?act=product-detail" class="text-dark">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</a></p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card p-3" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--products-->
+                    <div class="col-md-6 col-lg-4 col-xl-3 p-3">
+                        <div class="card p-3" style="width: 18rem;">
+                            <img src="./assets/img/products/laptop01.webp" class="card-img-top" alt="...">
+                            <div class="row">
+                                <div class="card-body ">
+                                    <p class="card-text fw-bold">Laptop gaming Acer Predator Helios 300 PH315 55
+                                        76KG</p>
+                                    <div class="text-start p-3">
+                                        <p class="card-text text-decoration-line-through fw-light">98.000.000 đ</p>
+                                        <p class="card-text">98.000.000 đ</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+    </section>
+
+</main><!-- End #main -->
+
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+
+</body>
+
+</html>
