45ec7839f29e56f442fb8f4065e540630cee4781
khaihoang <khaihoang119nkh@gmail.com>
2023-11-28 04:37:46 +0700

    Chỉnh sửa lại đường dẫn file

--- view/product-detail.php
+++ view/product-detail.php
@@ -27,13 +27,13 @@
                       </div>
                       <div class="carousel-inner">
                           <div class="carousel-item active">
-                              <img src="./assets/img/products/laptop01.webp" class="d-block w-100" alt="...">
+                              <img src="./assets/client/img/products/laptop01.webp" class="d-block w-100" alt="...">
                           </div>
                           <div class="carousel-item">
-                              <img src="./assets/img/products/laptop01.webp" class="d-block w-100" alt="...">
+                              <img src="./assets/client/img/products/laptop01.webp" class="d-block w-100" alt="...">
                           </div>
                           <div class="carousel-item">
-                              <img src="./assets/img/products/laptop01.webp" class="d-block w-100" alt="...">
+                              <img src="./assets/client/img/products/laptop01.webp" class="d-block w-100" alt="...">
                           </div>
                       </div>
                       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
