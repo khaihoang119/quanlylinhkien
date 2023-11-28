1a152d81a0bfa2b09b0d0dc55d3f827d6beb6e2f
khaihoang <khaihoang119nkh@gmail.com>
2023-11-28 17:15:21 +0700

    NO-01 chỉnh sửa giao diện website

--- view/account/edit-account.php
+++ view/account/edit-account.php
@@ -5,20 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="./assets/img/icons/favicon.ico"/>
+    <link rel="icon" type="image/png" href="./assets/client/img/icons/favicon.ico"/>
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="./assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/vendor/bootstrap/css/bootstrap.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="./assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="./assets/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/vendor/animate/animate.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="./assets/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/vendor/css-hamburgers/hamburgers.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="./assets/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/vendor/select2/select2.min.css">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="./assets/css/util.css">
-    <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/css/util.css">
+    <link rel="stylesheet" type="text/css" href="./assets/client/css/main.css">
     <!--===============================================================================================-->
 </head>
 <body>
@@ -27,16 +27,24 @@
     <div class="container-login100">
         <div class="wrap-login100">
             <div class="login100-pic js-tilt" data-tilt>
-                <img src="./assets/img/img-01.png" alt="IMG">
+                <img src="./assets/client/img/img-01.png" alt="IMG">
             </div>
 
             <form class="login100-form validate-form">
 					<span class="login100-form-title">
-						Đăng Kí
+						Sửa đổi thông tin tài khoản
 					</span>
 
                 <div class="wrap-input100 validate-input">
                     <input class="input100" type="text" name="userName" placeholder="Tên tài khoản">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+                </div>
+
+                <div class="wrap-input100 validate-input">
+                    <input class="input100" type="text" name="diaChi" placeholder="Địa chỉ">
                     <span class="focus-input100"></span>
                     <span class="symbol-input100">
 							<i class="fa fa-user" aria-hidden="true"></i>
@@ -70,37 +78,32 @@
 
                 <div class="container-login100-form-btn">
                     <button class="login100-form-btn">
-                        Đăng kí
+                        Cập nhật
                     </button>
                 </div>
 
-                <div class="text-center p-t-12">
-                    <a class="txt2" href="index.php?act=login">
-                        Bạn đã có tài khoản?
-                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                    </a>
-                </div>
+
             </form>
         </div>
     </div>
 </div>
 
 <!--===============================================================================================-->
-<script src="./assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="./assets/client/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-<script src="./assets/vendor/bootstrap/js/popper.js"></script>
-<script src="./assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="./assets/client/vendor/bootstrap/js/popper.js"></script>
+<script src="./assets/client/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-<script src="./assets/vendor/select2/select2.min.js"></script>
+<script src="./assets/client/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-<script src="./assets/vendor/tilt/tilt.jquery.min.js"></script>
+<script src="./assets/client/vendor/tilt/tilt.jquery.min.js"></script>
 <script >
     $('.js-tilt').tilt({
         scale: 1.1
     })
 </script>
 <!--===============================================================================================-->
-<script src="./assets/js/mainlogin.js"></script>
+<script src="./assets/client/js/mainlogin.js"></script>
 
 </body>
 </html>
--- view/product-detail.php
+++ view/product-detail.php
@@ -47,19 +47,46 @@
 
           <div class="col-lg-6">
             <div class="portfolio-info">
+                <h2>Tên sản phẩm</h2>
+                <h3>74.000.000 đ</h3>
               <h3>Thông tin sản phẩm</h3>
               <ul>
-                <li><strong>Category</strong>: Web design</li>
-                <li><strong>Client</strong>: ASU Company</li>
-                <li><strong>Project date</strong>: 01 March, 2020</li>
-                <li><strong>Project URL</strong>: <a href="#">www.example.com</a></li>
+                <li><strong>Nhà sản xuất</strong>: Web design</li>
+                <li><strong> Tình trạng</strong>: ASU Company</li>
+                <li><strong>Bảo hành</strong>: 01 March, 2020</li>
               </ul>
+                <form action="index.php?act=product" method="post">
+                    <input type="submit" name="" class="btn btn-primary" value="Thêm vào giỏ hàng">
+                </form>
             </div>
+              <div class="portfolio-description">
+                  <h2>Đánh giá chi tiết</h2>
+                  <p>
+                      HS55 Stereo là dòng sản phẩm tai nghe máy tính mới nhất đến từ Corsair với nhiều cải tiến về công nghệ âm thanh mang đến cho người dùng những trải nghiệm tuyệt vời trong những trò chơi mình yêu thích.
+                  </p>
+                  <p>
+                      Corsair được biết đến là một trong những hãng chuyên sản xuất các dòng sản phẩm gaming gear chất lượng được người dùng tiêu dùng đánh giá cao về hiệu năng lẫn thiết kế từ bàn phím, chuột máy tính, tai nghe cho đến ghế gaming.
+                      Tai nghe chơi game Corsair HS55 Stereo là một thiết bị ấn tượng mà bất kỳ game thủ bình thường hay chuyên nghiệp nào cũng nên yêu thích. Với thiết kế dạng over-ear (tai nghe chụp tai) mang đến âm thanh tốt, lọc tiếng ồn cực tốt.
+                  </p>
+              </div>
             <div class="portfolio-description">
-              <h2>This is an example of portfolio detail</h2>
+              <h2>Thông số chi tiết</h2>
               <p>
-                Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.
+                  CPU	Intel Core i9-13900H
               </p>
+                <p>
+                    RAM	32GB (16x2) DDR5 5200MHz
+                </p>
+                  <p>
+                      Card đồ họa	NVIDIA® GeForce RTX™ 4070 8GB GDDR6 + MUX Switch
+                  </p>
+                 <p>
+                     Màn hình	16" UHD+ (3840x2400), 16:10, 100% DCI-P3, OLED, DisplayHDR True Black 600
+                 </p>
+                <p>
+                    Hệ điều hành	Windows 11 Home
+                </p>
+
             </div>
           </div>
 
