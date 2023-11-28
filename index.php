ed3268f21b7db6aa47c0c569f5900984d906b8f5
khaihoang <khaihoang119nkh@gmail.com>
2023-11-28 14:56:33 +0700

    NO-01 chỉnh sửa css

--- index.php
+++ index.php
@@ -40,36 +40,36 @@ include './view/footer.php';
     <meta content="" name="description">
     <meta content="" name="keywords">
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="assets/client/img/favicon.png" rel="icon">
+    <link href="assets/client/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/client/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/client/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/client/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/client/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/client/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/client/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/client/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/style2.css" rel="stylesheet">
+    <link href="assets/client/css/style.css" rel="stylesheet">
+    <link href="assets/client/css/style2.css" rel="stylesheet">
 </head>
 <body>
-<script src="assets/vendor/aos/aos.js"></script>
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/client/vendor/aos/aos.js"></script>
+<script src="assets/client/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/client/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/client/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/client/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/client/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="assets/client/vendor/php-email-form/validate.js"></script>
 
 <!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
+<script src="assets/client/js/main.js"></script>
 
 </body>
 </html>
