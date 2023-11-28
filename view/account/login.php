1bc7da1599a05b4f88087144929e2a449356c229
Johhny <khaihoang119nkh@gmail.com>
2023-11-28 08:44:28 +0700

    NO1-chinh sua giao dien

--- view/account/login.php
+++ view/account/login.php
@@ -5,20 +5,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="./assets/img/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="./assets/client/img/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="./assets/vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="./assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="./assets/vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/vendor/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="./assets/vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="./assets/vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/vendor/select2/select2.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="./assets/css/util.css">
-	<link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/css/util.css">
+	<link rel="stylesheet" type="text/css" href="./assets/client/css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
@@ -27,7 +27,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="./assets/img/img-01.png" alt="IMG">
+					<img src="./assets/client/img/img-01.png" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form">
@@ -77,21 +77,21 @@
 	</div>
 
 <!--===============================================================================================-->	
-	<script src="./assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="./assets/client/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="./assets/vendor/bootstrap/js/popper.js"></script>
-	<script src="./assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="./assets/client/vendor/bootstrap/js/popper.js"></script>
+	<script src="./assets/client/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="./assets/vendor/select2/select2.min.js"></script>
+	<script src="./assets/client/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="./assets/vendor/tilt/tilt.jquery.min.js"></script>
+	<script src="./assets/client/vendor/tilt/tilt.jquery.min.js"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
 <!--===============================================================================================-->
-	<script src="./assets/js/mainlogin.js"></script>
+	<script src="./assets/client/js/mainlogin.js"></script>
 
 </body>
 </html>
